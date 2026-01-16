6b1e423633bac0b4bbace946572c203019e26863
yuvaraj1916 <gyuvarajcrl@gmail.com>
2026-01-16 13:11:45 +0530

    Remove Day 3 and Day 4 files

--- day3_sum.py
+++
@@ -1,4 +0,0 @@
-# write a program to print the sum of two numbers
-a = 1234
-b = 5678
-print(a + b)
--- day4_difference.py
+++
@@ -1,4 +0,0 @@
-# write a program to print the difference of two numbers
-a = 3456
-b = 1234
-print(a - b)
