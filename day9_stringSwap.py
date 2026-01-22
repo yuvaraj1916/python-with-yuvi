4304c3327d2ba418b8c9db4fd240d8512d39137d
yuvaraj1916 <gyuvarajcrl@gmail.com>
2026-01-22 11:14:33 +0530

    Add string swap input-output program

---
+++ day9_stringSwap.py
@@ -0,0 +1,5 @@
+#Write a Python program that accepts two strings from the user and displays them by printing the second string first and the first string next
+word1 = input("Enter the first string: ")
+word2 = input("Enter the second String: ")
+print(word2)
+print(word1)
