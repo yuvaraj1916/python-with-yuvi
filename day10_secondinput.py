b71d5e10e1b3676332470f270ba6817577147358
yuvaraj1916 <gyuvarajcrl@gmail.com>
2026-01-25 21:38:30 +0530

    Day 10: Print only the second input

---
+++ day10_secondinput.py
@@ -0,0 +1,4 @@
+# write a program that reads 2 inputs and print only the second input
+input1 = input("Enter the first input: ")
+input2 = input("Enter the second input: ")
+print("The second input is:", input2)
