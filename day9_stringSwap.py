b02de671b5006504f881c9526c09d57e35f5e9de
yuvaraj1916 <gyuvarajcrl@gmail.com>
2026-01-25 21:11:10 +0530

    Delete day9_stringSwap.py

--- day9_stringSwap.py
+++
@@ -1,5 +0,0 @@
-#Write a Python program that accepts two strings from the user and displays them by printing the second string first and the first string next
-word1 = input("Enter the first string: ")
-word2 = input("Enter the second String: ")
-print(word2)
-print(word1)
