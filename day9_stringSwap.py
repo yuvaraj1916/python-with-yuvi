c901c75e0092da4c5b41cba4ff2e5c865fd284bf
yuvaraj1916 <gyuvarajcrl@gmail.com>
2026-01-25 21:18:30 +0530

    Day 9: String swapping

--- day9_stringSwap.py
+++ day9_stringSwap.py
@@ -1,5 +1,6 @@
-#Write a Python program that accepts two strings from the user and displays them by printing the second string first and the first string next
-word1 = input("Enter the first string: ")
-word2 = input("Enter the second String: ")
-print(word2)
-print(word1)
+# write a program that take 2 inputs and print thw strings in swapped order
+str1 = input("Enter the first string: ")
+str2 = input("Enter the second string: ")
+print("Swapped strings:")
+print(str2)
+print(str1)
