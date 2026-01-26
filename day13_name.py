01b79426045bca4141baea1b49d36b3f30858427
yuvaraj1916 <gyuvarajcrl@gmail.com>
2026-01-26 18:23:31 +0530

    Day 13: printing name and age in a scentence

---
+++ day13_name.py
@@ -0,0 +1,4 @@
+# write a program that reads name and age of a person and prints them in a sentence
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print(name + " is " + str(age) + " years old.")
