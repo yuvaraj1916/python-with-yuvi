d62c38414bedff048e53b5358d490af260d35bec
yuvaraj1916 <gyuvarajcrl@gmail.com>
2026-01-26 18:19:26 +0530

    Day 12: Joining words with space

---
+++ day12_joinwords.py
@@ -0,0 +1,5 @@
+#write a program that takes two words as input and joins them with a space in between
+word1 = input("Enter the first word: ")
+word2 = input("Enter the second word: ")
+joined_words = word1 + " " + word2
+print("Joined Words: ", joined_words)
