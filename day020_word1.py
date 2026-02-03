301b65b5ec35eac1048da59bc6c00e108e8a964c
yuvaraj1916 <gyuvarajcrl@gmail.com>
2026-02-03 20:34:37 +0530

    Create day020_word1.py
    Day 020: printing the word in reverse order

---
+++ day020_word1.py
@@ -0,0 +1,7 @@
+# write a program that reads two words a and b , and prints the given word in reverse order separated by "###"
+""" if the input is A and B 
+then output should be like b###a"""
+
+a = input("Enter first word: ")
+b = input("Enter second word: ")
+print(b + "###" + a)
