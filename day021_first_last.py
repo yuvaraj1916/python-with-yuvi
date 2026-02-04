fe066a12094d22d1b5f8429aaa1f5777f02b691d
yuvaraj1916 <gyuvarajcrl@gmail.com>
2026-02-04 10:28:00 +0530

    Create day021_first_last.py
    Day 021: printing the first and last character of the word

---
+++ day021_first_last.py
@@ -0,0 +1,4 @@
+# write a program that reads a word and print the first and last character of the word in 2 lines 
+word = input("Enter a word: ")
+print("The first character is:", word[0])
+print("The last character is:", word[-1])
