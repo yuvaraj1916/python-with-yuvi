d1cf9896ee95c1a7972bae0d3c10cd12227d4980
yuvaraj1916 <gyuvarajcrl@gmail.com>
2026-02-05 19:16:32 +0530

    Create day022_string1.py
    Day 022: Printing he first characters of the string

---
+++ day022_string1.py
@@ -0,0 +1,8 @@
+# you are given three strings and print the first character of each string
+word1 = input("Enter the first word: ")
+word2 = input("Enter the second word: ")
+word3 = input("Enter the third word: ")
+word1_index = word1[0]
+word2_index = word2[0]
+word3_index = word3[0]
+print(word1_index + word2_index + word3_index) 
