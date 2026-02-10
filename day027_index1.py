eae631a67a8349e758eb7306856f031762890e76
yuvaraj1916 <gyuvarajcrl@gmail.com>
2026-02-10 09:35:05 +0530

    Create day027_index1.py
    Day 027: printing the last index of the word

---
+++ day027_index1.py
@@ -0,0 +1,5 @@
+# write a program that reads a words and prints the index of last character of the word 
+word = input("Enter a word: ")
+length = len(word)
+last_index = length - 1
+print(last_index)
