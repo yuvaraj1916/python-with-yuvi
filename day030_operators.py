b95e41f437f151ae051a3fc08fb03db08d98bcb6
yuvaraj1916 <gyuvarajcrl@gmail.com>
2026-02-13 19:09:02 +0530

    Create day030_operators.py
    Day 030 : performing mathematical operations

---
+++ day030_operators.py
@@ -0,0 +1,11 @@
+# write a program to perform addition, subtraction, multiplication, and division of two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+addition = num1 + num2 
+subtraction = num1 - num2 
+multiplication = num1 * num2
+division = num1 / num2 
+print("Addition:", addition)
+print("subtraction:", subtraction)
+print("multiplication:", multiplication)
+print("division:", division)
