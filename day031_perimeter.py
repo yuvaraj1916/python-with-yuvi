579630d9c1e9938822187d6ff0b3a1a132d96925
yuvaraj1916 <gyuvarajcrl@gmail.com>
2026-02-15 20:57:07 +0530

    Create day031_perimeter.py
    Day 032 : perimeter of rectangle

---
+++ day031_perimeter.py
@@ -0,0 +1,5 @@
+# write a program to calculate perimeter of rectangle
+length = int(input("Enter the length of the Rectangle: "))
+width = int(input("Enter the width of the Rectangle: "))
+perimeter = 2 * (length + width)
+print("The perimeter of the Rectangle is: " + str(perimeter))
