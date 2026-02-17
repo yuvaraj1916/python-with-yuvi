936f3bf74b55024de2127a775d99ad77fab8cb6f
yuvaraj1916 <gyuvarajcrl@gmail.com>
2026-02-17 09:08:05 +0530

    Create day033_kilometer.py
    Day 033 : converting kilometers into meters

---
+++ day033_kilometer.py
@@ -0,0 +1,4 @@
+# write a program that take kilometer as input and convert into meters and print the number of meters
+kilometer = float(input("Enter the distance in kilometers: "))
+meters = int(kilometer * 1000)
+print("The distance in meters is: " + str(meters)+ " meters")
