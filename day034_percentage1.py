5a488af2ee1bf95a97035669f2399faf6a5a3278
yuvaraj1916 <gyuvarajcrl@gmail.com>
2026-02-18 10:15:15 +0530

    Create day034_percentage1.py
    Day 034 : percentage of 200

---
+++ day034_percentage1.py
@@ -0,0 +1,4 @@
+# write a program that reads the percentage p and prints the percentage p of number 200.
+percentage = float(input("Enter the percentage: "))
+result = (percentage / 100)* 200
+print("Percentage", percentage, "of 200 is:", result)
