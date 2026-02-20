85a1fdc8a12b1191271f907c88dce1c2d9711a59
yuvaraj1916 <gyuvarajcrl@gmail.com>
2026-02-20 16:33:05 +0530

    Create day035_remainder.py
    day035 : printing the remainder

---
+++ day035_remainder.py
@@ -0,0 +1,5 @@
+# write a program that reads a dividend and divisor and prints the remainder.
+dividend = int(input("Enter the dividend: "))
+divisor = int(input("Enter the divisor: "))
+remainder = dividend % divisor 
+print("The remainder of", dividend, "divided by", divisor, "is:", remainder)
