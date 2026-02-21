0b796f43bf5502d38606495e639ce632b7a92e32
yuvaraj1916 <gyuvarajcrl@gmail.com>
2026-02-21 09:51:05 +0530

    Create day036_sum.py
    Day 036: sum of given string

---
+++ day036_sum.py
@@ -0,0 +1,6 @@
+# write a program that reads a string prints the sum of  digits of given 3 digit number
+number = input("Enter a 3 digit number: ")
+one = int(number[0])
+two = int(number[1])
+three = int(number[2])
+print("The sum of the digits is: ",one + two + three)
