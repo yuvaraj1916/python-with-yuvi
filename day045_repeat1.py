66e24271652891916f909b80778cebc9d3a130ea
yuvaraj1916 <gyuvarajcrl@gmail.com>
2026-03-03 20:09:01 +0530

    Create day045_repeat1.py
    Day 045: repeat the string n times

---
+++ day045_repeat1.py
@@ -0,0 +1,5 @@
+# write a program that reads a string and a number n. Then repeat the string n times seperated by space.
+string = input("Enter a string: ")
+n = int(input("Enter a number: "))
+result = (string+" ")*n 
+print(result)
