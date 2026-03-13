df48b26d52bc5b52c5314d49d259e327ef670a89
yuvaraj1916 <gyuvarajcrl@gmail.com>
2026-03-13 17:28:50 +0530

    Create day053_greater60.py
    Day 053: checking a number greater than 60 or not

---
+++ day053_greater60.py
@@ -0,0 +1,7 @@
+# write a program that reads a string and check if the number is greater than 60 or not
+number_str = input("Enter a number: ")
+number = int(number_str)
+if number > 60:
+    print("The number is greater than 60.")
+else:
+    print("The number is not greater than 60.")
