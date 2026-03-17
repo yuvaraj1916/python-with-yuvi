e6af2413bd82f194379f8c54f78f44507bd82b9a
yuvaraj1916 <gyuvarajcrl@gmail.com>
2026-03-17 18:12:34 +0530

    Create day055_negative.py
    Day 055: checking the number is negative or not

---
+++ day055_negative.py
@@ -0,0 +1,8 @@
+# write a program that reads a number and check the number is negative
+number = int(input("Enter a number: "))
+if number < 0:
+    print("The number is negative.")
+elif number > 0:
+    print("The number is positive.")
+else:
+    print("The number is 0.")
