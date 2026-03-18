0d9a32aee97c65b57026a578144cd73ba7ea51a7
yuvaraj1916 <gyuvarajcrl@gmail.com>
2026-03-18 18:48:42 +0530

    Create day056_equal.py
    Day 056: checking if 2 strings are equal or not.

---
+++ day056_equal.py
@@ -0,0 +1,7 @@
+# write a program that reads two strings and checks if they are equal
+string1 = input("Enter the first string: ")
+string2 = input("Enter the second string: ")
+if string1 == string2:
+    print("The strings are equal.")
+else:
+    print("The strings are not equal.")
