cf5cde9fa6a44df1802cd75388f9ca62c55e0162
yuvaraj1916 <gyuvarajcrl@gmail.com>
2026-03-20 09:37:34 +0530

    Create day057_notequal.py
    Day 057: checking if the numbers are not equal

---
+++ day057_notequal.py
@@ -0,0 +1,7 @@
+# write a program that reads two numbers and check if they are not equal 
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+if number1 != number2:
+    print("The numbers are not equal.")
+else:
+    print("The numbers are equal.")
