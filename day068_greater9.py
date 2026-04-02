6232c89cfffd82f01722605dea6dabc8c4b2a8a9
yuvaraj1916 <gyuvarajcrl@gmail.com>
2026-04-02 21:12:58 +0530

    Create day068_greater9.py
    Day 068: Program checks whether both input numbers are greater than nine.

---
+++ day068_greater9.py
@@ -0,0 +1,7 @@
+# write a program that reads two numbers A and B , and checks if both given numbers are greater than 9.
+A = int(input("Enter the first number: "))
+B = int(input("Enter the second number: "))
+if A > 9 and B > 9:
+    print("Both numbers are greater than 9.")
+else:
+    print("Both numbers are not greater than 9.")
