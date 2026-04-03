72f48672ffcfcd7baba4dd09b4575e7f9fc0d333
yuvaraj1916 <gyuvarajcrl@gmail.com>
2026-04-03 21:15:47 +0530

    Create day069_negative.py
    Day 069: Program checks whether both input numbers are negative values.

---
+++ day069_negative.py
@@ -0,0 +1,7 @@
+# write a program that reads two numbers A and B , and checks if both given numbers are negative.
+A = int(input("Enter the first number: "))
+B = int(input("Enter the second number: "))
+if A < 0 and B < 0:
+    print("Both numbers are negative.")
+else:
+    print("Both numbers are not negative.")
