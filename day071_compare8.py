9aec61109a9413a8e7644e626e8547e435256f79
yuvaraj1916 <gyuvarajcrl@gmail.com>
2026-04-05 12:03:58 +0530

    Create day071_compare8.py
    Day 071: Program checks if input number lies between 25 and 75.

---
+++ day071_compare8.py
@@ -0,0 +1,6 @@
+# write a program that reads a number and checks if given number is between 25 and 75.
+number = int(input("Enter a number: "))
+if 25 < number < 75:
+    print("The number is between 25 and 75.")
+else:
+    print("The number is not between 25 and 75.")
