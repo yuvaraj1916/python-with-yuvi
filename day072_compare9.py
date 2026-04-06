c106ce1bccff9c6181b86682ffb5fca9d3bb9b84
yuvaraj1916 <gyuvarajcrl@gmail.com>
2026-04-06 19:10:02 +0530

    Create day072_compare9.py
    Day 072: Program checks whether any of three input strings equals “Men”.

---
+++ day072_compare9.py
@@ -0,0 +1,8 @@
+# write a program that reads three strings A,B and C and checks if one of the given string is equal to Men.
+A = input("Enter string A: ")
+B = input("Enter string B: ")
+C = input("Enter string C: ")
+if A == "Men" or B == "Men" or C == "Men":
+    print("One of the given strings is equal to Men.")
+else:
+    print("None of the given strings is equal to Men.")
